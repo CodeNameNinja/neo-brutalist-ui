467424ae6acd45d0f7ba9ab89557004d4001282d
Mitchell Yuen <mwhyuen@gmail.com>
2024-06-06 21:11:10 +0200

    feat: add storybook

--- packages/nb-ui-native/.babelrc.js
+++ packages/nb-ui-native/.babelrc.js
@@ -1,23 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
-
   return {
-    presets: [
-      [
-        '@nx/react/babel',
-        {
-          runtime: 'automatic',
-          useBuiltIns: 'usage',
-        },
-      ],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
     ],
-    plugins: [],
-    env: {
-      test: {
-        presets: [
-          ['module:@react-native/babel-preset', { useTransformReactJSX: true }],
-        ],
-      },
-    },
   };
 };
